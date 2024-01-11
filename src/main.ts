2af7c38de5006d2c6316a243271a579525501ed5
Oscar Botteri <oscar.botteri@rocketinsights.com>
2024-01-11 17:47:52 -0300

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import compression from 'compression';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
@@ -9,6 +10,11 @@ import { HttpLoggingInterceptor } from './common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get<ConfigService>(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('API docs')
+    .setDescription('API description')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
   // This will cause class-validator to use nestJS module resolution
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
@@ -35,6 +41,9 @@ async function bootstrap() {
 
   // Starts listening for shutdown hooks
   app.enableShutdownHooks();
+
+  // Mounts Swagger docs at /docs
+  SwaggerModule.setup('docs', app, document);
 
   // Starts API listening on the desidered port
   await app.listen(configService.get<number>('APP_PORT'));
